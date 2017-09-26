bfa8d7faf4a8164efde11cc1a8a1b292974f8897
Nadav Recca <nadavoosh@users.noreply.github.com>
2017-09-26 10:11:51 -0700

    Title iTerm window (#6)
    * add devvpn command even though it's a bit custom
    
    * mm

--- system/alias.sh
+++ system/alias.sh
@@ -1,3 +1,7 @@
 alias ..="cd .."
 alias reload="source ~/.bash_profile"
-
+alias devvpn="sudo /usr/local/Cellar/openvpn/2.3.7/sbin/openvpn ~/Downloads/devclient.ovpn"
+function title {
+    echo -ne "\033]0;"$*"\007"
+}
+alias dotfiles="cd ~/.dotfiles"
