83c401c2d53b021c839b48fd4987d9ee69775fe3
Nadav Recca <nadav@blend.com>
2018-01-22 13:59:13 -0800

    update more global settings for git

--- git/install.sh
+++ git/install.sh
@@ -1,4 +1,6 @@
 # install git bash support
 curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > $DOTFILES_DIR/git/git-prompt.sh
-# create global .gitignore file
+# update global configs
 git config --global core.excludesfile ~/.gitignore_global
+git config --global user.name "Nadav Recca"
+git config --global user.email nadav@blend.com
