087616ee112d252600bf9a730c070cd8f74e323f
Nadav Recca <nadav@blend.com>
2018-01-23 13:57:22 -0800

    add oh-my-zsh support

---
+++ oh-my-zsh/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# get powerline fonts
+pip install --user powerline-status
+# install a font
+https://github.com/powerline/fonts
