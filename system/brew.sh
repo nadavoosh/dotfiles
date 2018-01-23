4ed68cfda4a19bcb56f1d95f78bcd766312d6d5e
Nadav Recca <nadav@blend.com>
2018-01-23 14:11:07 -0800

    cleanup brew file

--- system/brew.sh
+++ system/brew.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-
+# Install command-line tools
 xcode-select --install
-
-# Install command-line tools using Homebrew
 
 # Make sure we’re using the latest Homebrew
 brew update
@@ -12,7 +10,19 @@ brew update
 brew upgrade
 
 brew install python3
-brew install iterm2
+brew tap caskroom/cask
+
+# install casks
+cask_args appdir: '/Applications'
+brew cask install google-chrome
+brew cask install 1password
+brew cask install spotify
+brew cask install fantastical
+brew cask install iterm2
+#brew cask install 'airmail'
+#brew cask install 'bear'
+
 
 # Remove outdated versions from the cellar
+brew cask cleanup
 brew cleanup
